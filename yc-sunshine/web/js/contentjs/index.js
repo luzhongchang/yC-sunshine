467482a641896fa11d87c9fdec7f54caa7596447
git_lu <m15895632351@163.com>
2017-07-22 10:03:03 +0800

    #commit

--- yc-sunshine/web/js/contentjs/index.js
+++ yc-sunshine/web/js/contentjs/index.js
@@ -12,7 +12,8 @@ $(document).ready(function () {
         window.location.href='https://www.baidu.com'
     })
 
-
+    var animtionTime = 300;
+    var menuanimateTime =150;
 
 
         //默认第一tab
@@ -25,13 +26,13 @@ $(document).ready(function () {
         if(index == 1)
         {
             // $('.subMenu-dropdown').slideDown(500);
-            $('.subMenu-dropdown').fadeIn(300);
+            $('.subMenu-dropdown').fadeIn(animtionTime);
 
         }
         else
         {
             // $('.subMenu-dropdown').slideUp(500);
-            $('.subMenu-dropdown').fadeOut(300);
+            $('.subMenu-dropdown').fadeOut(animtionTime);
         }
     })
     $('.menu-list').click(function () {
@@ -58,11 +59,61 @@ $(document).ready(function () {
     $(".sub-menu li").mouseenter(function () {
         $(".sub-menu li").css({'color':'#ff00ff'});
         $(".sub-menu li").not($(this)).css({'color': 'rgba(0, 0, 0, 0.8)'});
-        $(this).animate({'margin-left':'10px'},150)
+        $(this).animate({'margin-left':'10px'},menuanimateTime)
 
-        $('.sub-menu li').not($(this)).animate({'margin-left':'0px'},150);
+        $('.sub-menu li').not($(this)).animate({'margin-left':'0px'},menuanimateTime);
 
     });
+
+    //黄灯片
+        $('.testSlider3').slider({
+		width:1286,
+		height:415,
+        durantion:2000,
+		originPosition:'center',
+	});
+
+
+    //         width: 243px;
+    // height: 243px;
+    // position: relative;
+    // left: 50%;
+    // top:50%;
+    // margin-left: -121.5px;
+    // margin-top: -121.5px;
+
+    $('.productimg img').mouseenter(function () {
+
+        $(this).animate({width:'265px',
+                        height:'265px',
+                        left: '50%',
+                        top:'50%',
+                        'margin-left': '-132.5px',
+                        'margin-top' :'-132.5px',
+        },animtionTime,'swing')
+
+    })
+    $('.productimg img').mouseout(function () {
+
+        $(this).animate({'width':'243px',
+                        'height':'243px',
+                        'left': '50%',
+                        'top':'50%',
+                        'margin-left': '-121.5px',
+                        'margin-top' :'-121.5px',
+
+        },animtionTime,'swing')
+
+    })
+
+    $('.description_product_a').hover(
+        function () {
+            $(this).css({'text-decoration': 'underline'});
+        },
+        function () {
+            $(this).css({'text-decoration':'none'});
+        }
+    )
 
 
 
