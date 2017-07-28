49280723fbcc8b908ee16b644ed2f56f9154d41d
git_lu <m15895632351@163.com>
2017-07-28 09:57:57 +0800

    #comit

---
+++ yc-sunshine/web/js/contentjs/about.js
@@ -0,0 +1,21 @@
+/**
+ * Created by luzhongchang on 17/7/27.
+ */
+$(document).ready(function () {
+
+    $('.packing-home-span').hover(
+        function () {
+            $(this).css({ 'border-bottom': '1px solid white'});
+        },
+        function () {
+            $(this).css({'border-bottom':'0px solid white'});
+        }
+    );
+    $('.packing-home-span').click(function () {
+        window.location.href='index.html';
+    });
+
+
+    new Function(selectItemMenu(3));
+
+});
